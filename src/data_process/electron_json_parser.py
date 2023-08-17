ce0ce30b0fa41055d1c462d824a3ac5d0855c231
jinbridge <2635480475@qq.com>
2023-08-18 00:54:53 +0800

    written private trigger/action

--- src/data_process/electron_json_parser.py
+++ src/data_process/electron_json_parser.py
@@ -4,17 +4,24 @@ import os
 
 class ElectronJsonParser:
     def __init__(
-        self, in_trigger_folder, in_action_folder, out_trigger_folder, out_action_folder
+        self,
+        in_trigger_folder,
+        in_action_folder,
+        out_trigger_folder,
+        out_action_folder,
+        private_path,
     ) -> None:
         self.in_trigger_folder = in_trigger_folder
         self.in_action_folder = in_action_folder
 
         self.out_trigger_folder = out_trigger_folder
         self.out_action_folder = out_action_folder
+        self.private_path = private_path
 
     def run(self):
-        self.handle_all(self.in_trigger_folder, self.out_trigger_folder, "trigger")
-        self.handle_all(self.in_action_folder, self.out_action_folder, "action")
+        # self.handle_all(self.in_trigger_folder, self.out_trigger_folder, "trigger")
+        # self.handle_all(self.in_action_folder, self.out_action_folder, "action")
+        self.parse_private(self.private_path)
 
     def handle_all(self, input_dir, output_dir, type):
         filenames = os.listdir(input_dir)
@@ -42,14 +49,37 @@ class ElectronJsonParser:
         output = {"names": item_names}
         return output
 
+    def parse_private(self, private_path):
+        with open(private_path) as f:
+            private_json = json.load(f)
+
+        items = private_json["data"]["me"]["private_channels"]
+        for item in items:
+            triggers = [
+                trigger_item["name"] for trigger_item in item["public_triggers"]
+            ]
+            actions = [action_item["name"] for action_item in item["public_actions"]]
+            name = item["name"]
+            output_trigger_str = json.dumps({"names": triggers}, indent=4)
+            with open(self.out_trigger_folder + name + ".json", "w+") as wf:
+                wf.write(output_trigger_str)
+            output_action_str = json.dumps({"names": actions}, indent=4)
+            with open(self.out_action_folder + name + ".json", "w+") as wf:
+                wf.write(output_action_str)
+
 
 if __name__ == "__main__":
     input_trigger_dir = "./data/trigger_json/"
     output_trigger_dir = "./data/electron_json/trigger/"
     input_action_dir = "./data/action_json/"
     output_action_dir = "./data/electron_json/action/"
+    private_json = "./data/private.json"
 
     ejp = ElectronJsonParser(
-        input_trigger_dir, input_action_dir, output_trigger_dir, output_action_dir
+        input_trigger_dir,
+        input_action_dir,
+        output_trigger_dir,
+        output_action_dir,
+        private_json,
     )
     ejp.run()
